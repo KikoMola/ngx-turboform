b6b140816724e0de9fc81e090785bd323cfdeeeb
Kiko <kikoguirado@gmail.com>
2025-04-25 13:46:56 +0200

    feat(i18n): implement dynamic language selection in AppComponent
    Added language selection functionality to the AppComponent by injecting the TranslateService. The constructor sets the default language to Spanish and determines the browser's language to provide a user-friendly experience. Supported languages include Spanish and English, ensuring that users can interact with the application in their preferred language. This enhancement improves the internationalization support and accessibility of the application, making it more adaptable to diverse user needs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-root',
@@ -8,5 +9,19 @@ import { RouterOutlet } from '@angular/router';
     styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ngx-turboform';
+  private translate = inject(TranslateService);
+
+  constructor() {
+    const defaultLang = 'es';
+    this.translate.setDefaultLang(defaultLang);
+
+    const supportedLangs = ['es', 'en'];
+    const browserLang = this.translate.getBrowserLang()?.split('-')[0];
+
+    const langToUse = browserLang && supportedLangs.includes(browserLang)
+                        ? browserLang
+                        : defaultLang;
+
+    this.translate.use(langToUse);
+  }
 }
